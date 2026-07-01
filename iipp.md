dd3a3c5a0b6e23fdffd08ad6c844a5b8e9d45a95
nlvle <106922287+nlvle@users.noreply.github.com>
2026-07-01 12:02:33 +0700

    add sim

---
+++ iipp.md
@@ -0,0 +1,18 @@
+\# ipp
+
+
+
+\## Features
+
+\- Mint NFT
+
+\- ERC721 Standard
+
+
+
+\## Tech Stack
+
+\- Solidity ^0.8.20
+
+\- Ethereum Compatible
+
---
+++ iipp.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SimpleNFT is ERC721, Ownable {
+    uint256 private _tokenIdCounter;
+
+    constructor() ERC721("MyWeb3NFT", "MWN") Ownable(msg.sender) {}
+
+    function mintNFT(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter++;
+        _safeMint(to, tokenId);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter;
+    }
+}
